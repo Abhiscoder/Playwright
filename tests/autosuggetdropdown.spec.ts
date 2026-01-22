12a880cf6c3e7bc5a8b72bf90c135956e0f2803c
Abhiscoder <abhishekkumar200m1@gmail.com>
2026-01-22 19:11:43 +0530

    autosuggestdorpdown

---
+++ tests/autosuggetdropdown.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect, Locator } from "@playwright/test";
+
+test("AutoSuggest dropdown", async ({ page }) => {
+  await page.goto("https://www.flipkart.com/");
+
+  await page.locator("input[name='q']").fill("smart"); // Search text
+  await page.waitForTimeout(5000);
+
+  //Get all the suggested options --> Ctrl+Shift+P on DOM --> emulate focused page
+  const options: Locator = page.locator("ul>li");
+
+  const count = await options.count();
+  console.log("Number of suggested options:", count);
+
+  //Printing all the suggested options in the console
+  console.log("Printing all suggestion");
+  for (let i = 0; i < count; i++) {
+    // console.log(await options.nth(i).innerText());
+    console.log(await options.nth(i).textContent());
+  }
+
+  await page.waitForTimeout(2000);
+});
